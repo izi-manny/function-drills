29b0dbd56367536cf8fb7e7ee670be9938fa295f
Emmanuel Izilov <em5750@gmail.com>
2021-09-23 00:05:38 -0400

    final version solutions

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -282,38 +282,32 @@ console.log(totalFrogs)
 
 //CODE HERE
 
-function numOfFrogs2(gold){
-  let frogPrice = 3;
-
-  let total = Math.floor(gold / frogPrice);
-  return total;
-}
-
-let totalFrogs = numOfFrogs2(10)
-console.log(totalFrogs)
+// Previous code addresses this scenario
 
 ////////////////// PROBLEM 15 ////////////////////
 let sampleArray = [0,1,2,3,4,7,5,6,8,9]
 /*
-  Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine if the array is in ascending order. 
-  The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set it equal to your function invoked. 
+  Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine 
+  if the array is in ascending order. 
+  The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, 
+  `arrayIsAscending` and set it equal to your function invoked. 
   Use the sample array to test this function.
 */
 
 //CODE HERE
-function sortedArr(arr){
+function isAscending(arr){  
   let arrIsSorted = true;
 
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] < arr[i + 1]) {
-      arrIsSorted;
+      return arrIsSorted;
     } else {
-      !(arrIsSorted)
+      return !(arrIsSorted)
     }
   }
 }
 
-let arrayIsAscending = sortedArr(sampleString);
+let arrayIsAscending = isAscending(sampleArray);
 console.log(arrayIsAscending)
 
 ////////////////// PROBLEM 16 ////////////////////
